61b42df1d44287fd96b0108eea314c840e59515a
monsef <abdomohammed929@gmail.com>
2020-04-13 22:23:35 +0200

    first

---
+++ app/Quiz.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Quiz extends Model
+{
+    protected $fillable = [
+        'name',
+    ];
+
+    public function question()
+    {
+        return $this->hasMany('App\Question');
+    }
+
+    public function course()
+    {
+        return $this->belongsTo('App\Course');
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany('App\User');
+    }
+}
